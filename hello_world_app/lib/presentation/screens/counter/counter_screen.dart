1510e93febc8d23958202c77c134f2f0145ba329
Derek Sesni Carreño <230892@utxicotepec.edu.mx>
2026-09-11 16:22:39 -0600

    Practica02
    Se agrego el CounterFunction Screen para la suma y reseteo de el contador

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
+  State<CounterScreen> createState() => _CounterScreenState();
+
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickcounter = 0;
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Counter Screen")));
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Counter Screen')
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Text(
+              '$clickcounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+             Text(
+              'Clicks${clickcounter==1? '' : 's'}',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickcounter+=1;  
+          });
+          
+        },
+      
+        child: const Icon(Icons.plus_one),
+      ),
+    );
   }
 }
